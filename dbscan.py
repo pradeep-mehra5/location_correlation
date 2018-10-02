d556e03b808390f711b5f205641fdc20857d0bf2
pradeep-mehra5 <mehra5pradeep@gmail.com>
2018-10-02 14:02:13 +0530

    comments improvement

--- dbscan.py
+++ dbscan.py
@@ -9,11 +9,11 @@ def _eps_neighborhood(p, q, eps):
     return getDistance(p[1],p[0],q[1],q[0])<eps;
 
 def _region_query(m, point_id, eps):
-    # n_points = m.shape[1]  #when executed as main
+    # n_points = m.shape[1]  #when using hardcoded numpy
     n_points = len(m[0])   #when used as a module
     seeds = []
     for i in range(0, n_points):
-        # if _eps_neighborhood(m[:, point_id], m[:, i], eps):  #when executed as main
+        # if _eps_neighborhood(m[:, point_id], m[:, i], eps):  #when using hardcoded numpy
         if _eps_neighborhood(list([m[0][point_id],m[1][point_id]]) , list([m[0][i],m[1][i]]) , eps): #when used as a module
             seeds.append(i)
     return seeds
@@ -46,11 +46,11 @@ def _expand_cluster(m, classifications, point_id, cluster_id, eps, min_points):
 
 def dbscan(m, eps, min_points):
     cluster_id = 1
-    # n_points = m.shape[1] #when executing in main
+    # n_points = m.shape[1] #when using hardcoded numpy
     n_points = len(m[1])  #when used as a module
     classifications = [UNCLASSIFIED] * n_points
     for point_id in range(0, n_points):
-        # point = m[:, point_id] #when executing as main
+        # point = m[:, point_id] #when using hardcoded numpy
         point = [m[0][point_id],m[1][point_id]]  #when used a module
         if classifications[point_id] == UNCLASSIFIED:
             if _expand_cluster(m, classifications, point_id, cluster_id, eps, min_points):
@@ -58,18 +58,10 @@ def dbscan(m, eps, min_points):
     return classifications
 
 def main():
-    # m = np.matrix('1 1.2 0.8 3.7 3.9 3.6 10; 1.1 0.8 1 4 3.9 4.1 10')
-    # set( [(0,1),(0,1),(1,0)] )
-    # m = np.matrix('39.978848289855065 39.981109225 39.98103225000002 39.97863413513517 39.98140922093023 40.0023006763285 ; 116.32662847826084 116.30907245 116.31030393478255 116.32625096396397 116.31101056976746 116.17073342028988 ')
-    m = [[39.978848289855065 , 39.981109225, 39.98103225000002, 39.97863413513517, 39.98140922093023,40.0023006763285], [116.32662847826084 ,116.30907245 ,116.31030393478255 ,116.32625096396397, 116.31101056976746 ,116.17073342028988]]
+    m = np.matrix('39.978848289855065 39.981109225 39.98103225000002 39.97863413513517 39.98140922093023 40.0023006763285 ; 116.32662847826084 116.30907245 116.31030393478255 116.32625096396397 116.31101056976746 116.17073342028988 ')
     eps = 200
     min_points = 2
-    #assert dbscan(m, eps, min_points) == [1, 1, 1, 2, 2, 2, None]
     print(dbscan(m, eps, min_points))
 
 if __name__ == '__main__':
     main()
-
-
-
-    #12212none
