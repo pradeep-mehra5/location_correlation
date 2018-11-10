d97a3d615b7494e334e034b33bab4cced347b169
pradeep-mehra5 <mehra5pradeep@gmail.com>
2018-11-10 20:26:46 +0530

    correlation excel file

--- mining_location_correlation.py
+++ mining_location_correlation.py
@@ -2,15 +2,15 @@ import global_vars
 import os
 import xlrd
 from auxiliary_fns import getTimeStamp
+import openpyxl
 
 def location_correlation():
     correlation = []
-    tripPartitionTime = 60*60*5
-    b = 1
+    # tripPartitionTime = 60*60*5
+    b = 2
     locationsNum = len(global_vars.LOCATION_DICT)
-    empty = [0]*(locationsNum+1)
-    for count in range(0,locationsNum+1):
-        correlation.append(empty)
+
+    correlation = [[0] * (locationsNum+1) for i in range(locationsNum+1)]
 
     for dirname,dirnames,filenames in os.walk('Location_History'):
         for filename in filenames:
@@ -28,19 +28,27 @@ def location_correlation():
                 else:
                     val = int(val)
                 history[key] = val
-            trips = TripPartition(history,tripPartitionTime)
+            trips = TripPartition(history)
             for trip in trips:
-                for index1 in range(len(trip)):
-                    for index2 in range(index1+1,len(trip)):
+                if len(trip)==0:
+                    continue
+                location_trip = []
+                location_trip.append(trip[0])
+                for index in range(1,len(trip)):
+                    if trip[index] == trip[index-1]:
+                        continue
+                    else:
+                        location_trip.append(trip[index])
+                for index1 in range(len(location_trip)):
+                    for index2 in range(index1+1,len(location_trip)):
                         alpha = b**(-1*(index2-index1-1))
-                        correlation[trip[index1]-1][trip[index2]-1] += alpha
+                        correlation[location_trip[index1]][location_trip[index2]] += alpha
     print(correlation)
-    return correlation
-
-# location_correlation()
+    createCorrelationFile(correlation)
 
 
-def TripPartition(history,tripPartitionTime = 1*60*60):
+
+def TripPartition(history,tripPartitionTime = 3*60*60):
     trips = []
     timeStampedHistory = {}
     for h in history:
@@ -75,7 +83,8 @@ def TripPartition(history,tripPartitionTime = 1*60*60):
         ids[positionOfMax] = temp
 
     a_trip = []
-    a_trip.append(ids[0])
+    if len(ids)>0:
+        a_trip.append(ids[0])
     for index in range(0,len(arrivals)-1):
         if arrivals[index+1]-leave[index] < tripPartitionTime:
             a_trip.append(ids[index+1])
@@ -86,6 +95,19 @@ def TripPartition(history,tripPartitionTime = 1*60*60):
     trips.append(a_trip)
     return trips
 
+
+def createCorrelationFile(correlation):
+    wb = openpyxl.Workbook()
+    correlationSheet = wb.active
+    correlationSheet.title = 'Correlation'
+    for i in range(2,len(correlation)+2):
+        correlationSheet.cell(row=i,column=1).value = i-2
+        correlationSheet.cell(row=1,column=i).value = i-2
+    for index1 in range(len(correlation)):
+        for index2 in range(len(correlation)):
+            correlationSheet.cell(row=2+index1,column=2+index2).value = correlation[index1][index2]
+    wb.save('Correlation.xlsx')
+    print(correlation)
 
 
 if __name__ == '__main__':
